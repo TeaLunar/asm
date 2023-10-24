6dfe589de082e18b9d10759b6fcbf4f1de6d3711
TeaLunar <143193419+TeaLunar@users.noreply.github.com>
2023-10-24 22:00:59 +0700

    Create cond.asm

---
+++ cond.asm
@@ -0,0 +1,33 @@
+format ELF64 executable 
+
+entry main
+
+main:
+    mov eax, 10
+    mov ebx, 11
+    cmp eax, ebx
+    jg hamster
+    
+    mov ecx, msgm
+    mov edx, lenl
+    jmp print_and_exit
+    
+hamster:
+    mov ecx, msg
+    mov edx, len
+    jmp print_and_exit
+    
+print_and_exit:
+    mov eax, 4
+    mov ebx, 1
+    int 0x80
+
+    mov eax, 1
+    xor ebx, ebx
+    int 0x80
+
+msg  db 'helloworld!', 0
+len = $-msg
+msgm  db 'ofwelqowd!', 0
+lenl = $-msgm
+  
